7fbd2e2acc9dbc1c832dfbb70ad6c01f3ca3753c
Chanwit Kaewkasi <chanwit@gmail.com>
2022-04-21 22:30:22 +0700

    use abspath

--- main.tf
+++ main.tf
@@ -58,14 +58,14 @@ resource "aws_s3_bucket" "lambda_bucket" {
 data "archive_file" "lambda_hello_world" {
   type = "zip"
 
-  source_dir = "${path.module}/hello-world"
-  output_path = "${path.module}/hello-world.zip"
+  source_dir = "${abspath(path.module)}/hello-world"
+  output_path = "${abspath(path.module)}/hello-world.zip"
 }
 
 resource "aws_s3_object" "lambda_hello_world" {
   bucket = aws_s3_bucket.lambda_bucket.id
 
-  key = "hello-world.zip"
+  key = "${abspath(path.module)}/hello-world.zip"
   source = data.archive_file.lambda_hello_world.output_path
 
   etag = filemd5(data.archive_file.lambda_hello_world.output_path)
