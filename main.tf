1d5cef8da3528e9beb9a9ed6036efbd56bef60b1
Chanwit Kaewkasi <chanwit@gmail.com>
2022-04-21 22:57:47 +0700

    upload zip

--- main.tf
+++ main.tf
@@ -54,21 +54,13 @@ resource "aws_s3_bucket" "lambda_bucket" {
   force_destroy = true
 }
 
-// Lambda function source
-data "archive_file" "lambda_hello_world" {
-  type = "zip"
-
-  source_dir = "${abspath(path.module)}/hello-world"
-  output_path = "${abspath(path.module)}/hello-world.zip"
-}
-
 resource "aws_s3_object" "lambda_hello_world" {
   bucket = aws_s3_bucket.lambda_bucket.id
 
-  key = "${abspath(path.module)}/hello-world.zip"
-  source = data.archive_file.lambda_hello_world.output_path
+  key = "hello-world.zip"
+  source = "${path.module}/hello-world.zip"
 
-  etag = filemd5(data.archive_file.lambda_hello_world.output_path)
+  etag = filemd5("${path.module}/hello-world.zip")
 }
 
 // Lambda function
@@ -95,7 +87,7 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
 
 resource "aws_lambda_function" "hello_world" {
   function_name = "HelloWorld"
-  description = "SteveW webinar"
+  description = "Chanwit Test tf-lambda"
 
   s3_bucket = aws_s3_bucket.lambda_bucket.id
   s3_key = aws_s3_object.lambda_hello_world.key
@@ -103,7 +95,7 @@ resource "aws_lambda_function" "hello_world" {
   runtime = "nodejs12.x"
   handler = "hello.handler"
 
-  source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
+  source_code_hash = filebase64sha256("${path.module}/hello-world.zip")
 
   role = aws_iam_role.lambda_exec.arn
 }
